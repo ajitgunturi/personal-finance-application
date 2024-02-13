9f9b5d0e8f75755a19914081c7faf21398631e0c
Ajit Kumar Gunturi <ajitg@vmware.com>
2024-02-13 21:21:08 +0530

    feat(commons): created commons module for holding domain and utils

---
+++ personal-finance-commons/src/main/java/com/arcane/pfa/commons/domain/UserDetails.java
@@ -0,0 +1,13 @@
+package com.arcane.pfa.commons.domain;
+
+import lombok.Data;
+
+@Data
+public class UserDetails {
+	
+	private String uid;
+	private String name;
+	private String email;
+	private String password;
+
+}
